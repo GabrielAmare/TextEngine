6d4f25af821a847eb5480081f7024e3f96acbc86
GabrielAmare <67603418+GabrielAmare@users.noreply.github.com>
2021-03-11 18:39:59 +0100

    Update Any.py
    patched Any behaviour

--- text_engine/core/Any.py
+++ text_engine/core/Any.py
@@ -1,20 +1,23 @@
-from ..base import Rule_List, Result_Unit, Result_Error
+from ..base import Rule_List, Result_Unit, Result_Error, Result_List
 from .Parser import Parser
 
 
 class Any(Rule_List):
     def parse(self, tokens: list, position: int, parser: Parser):
+        errors = Result_List(rule=self, at_position=position)
         for rule in self.rules:
             result = rule.parse(tokens, position, parser)
             if result:
                 return AnyResult(rule=self, result=result)
             else:
-                return Result_Error(
-                    rule=self,
-                    at_position=position,
-                    reason=f"No matching rule in Any",
-                    result=result,
-                )
+                errors.append(result)
+
+        return Result_Error(
+            rule=self,
+            at_position=position,
+            reason=f"No matching rule in Any",
+            result=errors,
+        )
 
 
 class AnyResult(Result_Unit):
