fbb14f7d6e5ae9cd0232c16cc41824a4f27086ee
GabrielAmare <67603418+GabrielAmare@users.noreply.github.com>
2021-03-12 14:16:49 +0100

    Update example_2.py

--- examples/example_2.py
+++ examples/example_2.py
@@ -34,6 +34,17 @@ class Decimal:
         return str(self.value)
 
 
+class Equations:
+    def __init__(self, *equations):
+        self.equations = equations
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(" + ",\n".join(map(repr, self.equations)) + ")"
+
+    def __str__(self):
+        return "\n".join(map(str, self.equations))
+
+
 class OP1B:
     seps: (str, str)
 
@@ -78,16 +89,34 @@ class Div(OP2N):
 
 
 class Pow(OP2N):
-    sep = "  "
+    sep = " ^ "
+
+
+class Attr(OP2N):
+    sep = " = "
 
 
 class BlocP(OP1B):
     seps = ("( ", " )")
 
 
+class Call:
+    def __init__(self, name, *args):
+        self.name = name
+        self.args = args
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}({repr(self.name)}, " + ", ".join(map(repr, self.args)) + ")"
+
+    def __str__(self):
+        return f"{str(self.name)}(" + ", ".join(map(str, self.args)) + ")"
+
+
 lexer, parser, builder, engine = base(
     # BLOCS
     [BlocP, match("LP") & match("L0").in_("*") & match("RP")],
+    [Attr, match("Id").in_("*") & match("EQUAL") & match("L0").in_("*")],
+    [Call, match("Id").in_("*") & match("LP") & match("L0").in_("*").sep_by("COMA", backward=True) & match("RP")],
     # OPERATORS
     [
         Pow,
@@ -106,6 +135,7 @@ lexer, parser, builder, engine = base(
     [Id, match("ID").in_("*")],
     [Integer, match("INTEGER").in_("*")],
     [Decimal, match("DECIMAL").in_("*")],
+    [Equations, match("LB").in_("*").sep_by("NEWLINE", backward=True)]
 
 )
 
@@ -114,6 +144,9 @@ lexer.add_pattern("MINUS", mode="str", expr="-")
 lexer.add_pattern("STAR", mode="str", expr="*")
 lexer.add_pattern("SLASH", mode="str", expr="/")
 lexer.add_pattern("HAT", mode="str", expr="^")
+lexer.add_pattern("EQUAL", mode="str", expr="=")
+lexer.add_pattern("NEWLINE", mode="str", expr="\n")
+lexer.add_pattern("COMA", mode="str", expr=",")
 
 lexer.add_pattern("LP", mode="str", expr="(")
 lexer.add_pattern("RP", mode="str", expr=")")
@@ -125,13 +158,18 @@ lexer.add_pattern("ID", mode="re", expr=r"[a-zA-Z_][a-zA-Z0-9_]*")
 lexer.add_pattern("DECIMAL", mode="re", expr=r"[0-9]+\.[0-9]*|\.[0-9]+", value=float)
 lexer.add_pattern("INTEGER", mode="re", expr=r"[0-9]+", value=int)
 
+parser.add_routine("LB", match("Attr") | match("L0"))
 parser.add_routine("L0", match("Add") | match("Sub") | match("L1"))
 parser.add_routine("L1", match("Mul") | match("Div") | match("L2"))
 parser.add_routine("L2", match("Pow") | match("L3"))
-parser.add_routine("L3", match("Id") | match("Integer") | match("Decimal") | match("BlocP"))
+parser.add_routine("L3", match("Call") | match("Id") | match("Integer") | match("Decimal") | match("BlocP"))
 
 if __name__ == "__main__":
-    text = "3 * x ^ 2 + 1 - 6 + 4"
+    text = """y = 3 * x ^ 2 + 1 - 6 + 4
+t = x² + y² + z²
+u = 2x - 3y + 7z
+v = 10x-6.7z + 1.001y
+f(x, x, x) * f(y, y, y) ^ f(z, z, z)"""
 
     result = engine.read(text, backward=True)
 
