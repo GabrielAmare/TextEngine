c886837a22d57b375aca28435f24ea6196a08bc0
GabrielAmare <67603418+GabrielAmare@users.noreply.github.com>
2021-03-13 21:02:06 +0100

    Update example_1.py

--- examples/example_1.py
+++ examples/example_1.py
@@ -2,7 +2,7 @@ from text_engine import *
 
 lexer, parser, builder, engine = base(pattern_libs=["blocs", "units"])
 
-lexer.add_pattern("COMMA", mode="str", expr=",")
+lexer.add_pattern("COMMA.SYMBOL", mode="str", expr=",")
 
 parser.add_routine("INT_ENUM", match("INT").in_("items").sep_by("COMMA", backward=True))
 
@@ -15,3 +15,18 @@ if __name__ == '__main__':
 
     ast = engine.read("(0, 15678, 95, 484876, 05959595)", backward=True)
     print(ast)
+
+    chart = {
+        "*": dict(bg="black", fg="white", bd=2, relief="sunken", font=("Consolas", 12, "bold"), padx=5, pady=5),
+        "ID": dict(fg="red"),
+        "INT": dict(fg="blue"),
+        "FLOAT": dict(fg="blue"),
+        "*.SYMBOL": dict(fg="orange"),
+        "*.POWER": dict(fg="purple"),
+    }
+    text = "(0, 15678, 95, 484876, 05959595)"
+
+
+    app = tkEngine(engine, text, chart, backward=True)
+
+    app.mainloop()
