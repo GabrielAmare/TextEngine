3ce6485787136ddcf8d3ca4466d2d86ca2d57234
GabrielAmare <67603418+GabrielAmare@users.noreply.github.com>
2021-03-13 21:00:20 +0100

    Create results.py
    regroup of the core results

---
+++ text_engine/core/results.py
@@ -0,0 +1,13 @@
+from .All import AllResult
+from .Any import AnyResult
+
+from .As import AsResult
+from .In import InResult
+
+from .Builder import BuilderResult
+from .Routine import RoutineResult
+
+from .Optional import OptionalResult
+from .Repeat import RepeatResult
+
+from .Match import MatchResult
