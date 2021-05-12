517e9193f17ae970aa841fdcdce0889aeb84d953
GabrielAmare <67603418+GabrielAmare@users.noreply.github.com>
2021-05-12 17:10:57 +0200

    removed item_sets package

--- item_sets/__init__.py
+++
@@ -1,5 +0,0 @@
-from .ItemSet import ItemSet
-from .ItemSetMap import ItemSetMap
-from .ItemSetSTD import ItemSetSTD
-from .ItemSetGlobal import ItemSetGlobal
-from .ItemSetLocal import ItemSetLocal
--- item_sets/functions.py
+++
@@ -1,8 +0,0 @@
-from typing import Generator
-
-__all__ = ["comb"]
-
-
-def comb(n: int) -> Generator[Generator[bool, None, None], None, None]:
-    for k in range(2 ** n):
-        yield (bool(k & 2 ** i) for i in range(n - 1, -1, -1))
