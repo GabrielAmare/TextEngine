8cd1422fce8354f0600eefa885e9acec5540693f
GabrielAmare <67603418+GabrielAmare@users.noreply.github.com>
2021-05-15 06:34:14 +0200

    rebuilt item_engine/example_1

--- examples/item_engine/example_1/maths/__init__.py
+++ examples/item_engine/example_1/maths/__init__.py
@@ -1,11 +1,13 @@
 from typing import Iterator
 from item_engine import *
+from item_engine.textbase.elements import Char, Token, Lemma
 from .lexer import lexer
 from .parser import parser
+
 
 __all__ = ['gen_networks']
 
 
 def gen_networks(lexer_cfg: dict, parser_cfg: dict) -> Iterator[Network]:
-    yield Network(function=lexer, **lexer_cfg)
-    yield ReflexiveNetwork(function=parser, **parser_cfg)
+    yield Network(function=lexer, input_cls=Char, output_cls=Token, to_ignore=['WHITESPACE'], **lexer_cfg)
+    yield ReflexiveNetwork(function=parser, input_cls=Token, output_cls=Lemma, to_ignore=[], **parser_cfg)
